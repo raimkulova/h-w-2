67bf210203e14c047c6fb9c09e3cfc6dcdcb9ec0
raimkulova <105550553+raimkulova@users.noreply.github.com>
2022-06-05 00:01:40 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,66 @@
+var sum = 10;
+if (sum == 10) {
+	alert('Верно');
+} else {
+	alert('Неверно');
+};
+
+
+
+
+let a = 0
+if (a == 0) {
+alert('Верно!');
+} else {
+alert('Неверно!');
+};
+
+
+
+
+if (num) {
+alert('Верно!');
+} else {
+alert('Неверно!');
+};
+
+let test = 0;
+if (test != true) {
+alert('Верно!');
+} else {
+alert('Неверно!');
+}
+
+
+
+
+
+let y = 5;
+let b = 10;
+if ((y > 2 && y < 11) || (b >= 6 && b < 14)) {
+console.log('Верно!');
+} else {
+console.log('Неверно!');
+};
+
+
+
+
+let num = 4;
+let result = '';
+switch (num) {
+case 1:
+result = 'Зима';
+break;
+case 2:
+result = 'Весна';
+break;
+case 3:
+result = 'Лето';
+break;
+case 4:
+result = 'Осень';
+break;
+}
+console.log(result);
+
